d93f0ea2bef37beb61e76bf964e63fbac1afb26f
Mark Hobson <mark.hobson@blackpepper.co.uk>
2015-11-18 10:44:34 +0000

    Imported script

---
+++ thymeleaf-fragment.js
@@ -0,0 +1,73 @@
+/*
+ * Includes Thymeleaf fragments client-side for prototyping purposes.
+ *
+ * Requires jQuery.
+ *
+ * TODO: Support nested includes
+ * TODO: Make prefix/suffix externally configurable
+ */
+
+// Prefix that gets prepended to view names when building a URL
+var templatePrefix = "../templates/";
+
+// Suffix that gets appended to view names when building a URL
+var templateSuffix = ".html";
+
+processAttributes();
+
+function processAttributes() {
+	// Hold off scripts waiting for the document to be ready
+	$.holdReady(true);
+	
+	var promises = [];
+	
+	$("[th\\:include]").each(function() {
+		var fragmentSpec = $(this).attr("th:include");
+		var fragmentUri = resolveFragmentUri(fragmentSpec);
+		promises.push(createLoadPromise(this, fragmentUri, false));
+	});
+	
+	$("[th\\:replace]").each(function() {
+		var fragmentSpec = $(this).attr("th:replace");
+		var fragmentUri = resolveFragmentUri(fragmentSpec);
+		promises.push(createLoadPromise(this, fragmentUri, true));
+	});
+	
+	// Release scripts once all fragments have been processed
+	$.when.apply(null, promises).done(function() {
+		$.holdReady(false);
+	});
+}
+
+function resolveFragmentUri(fragmentSpec) {
+	var tokens = fragmentSpec.split("::");
+	var templateName = tokens[0];
+	var fragmentName = removeFragmentParameters(tokens[1]);
+	
+	var resourceName = resolveTemplate(templateName);
+	var fragmentSelector = "[th\\:fragment^='" + fragmentName + "']";
+
+	return resourceName + " " + fragmentSelector;
+}
+
+function removeFragmentParameters(fragmentExpression) {
+	var index = fragmentExpression.indexOf("(");
+	return (index == -1) ? fragmentExpression : fragmentExpression.substr(0, index);
+}
+
+function resolveTemplate(templateName) {
+	var link = document.createElement("a");
+	link.href = templatePrefix + templateName + templateSuffix;
+	return link.href;
+}
+
+function createLoadPromise(element, url, replace) {
+	return $.Deferred(function(deferred) {
+		$(element).load(url, function() {
+			if (replace) {
+				$(element).children().first().unwrap();
+			}
+			deferred.resolve();
+		});
+	}).promise();
+}
