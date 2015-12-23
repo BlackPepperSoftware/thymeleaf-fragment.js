a0527ade3f493d9feae424cc859af6953662706f
Mark Hobson <mark.hobson@blackpepper.co.uk>
2015-12-23 17:00:02 +0000

    Issue #3: Renamed include test page

--- test/thymeleaf-fragment-test.js
+++ test/thymeleaf-fragment-test.js
@@ -1,7 +1,7 @@
 describe("thymeleaf-fragment.js", function() {
 	
 	it("should include fragments", function() {
-		browser.driver.get("http://localhost:8080/test/thymeleaf-fragment-test.html");
+		browser.driver.get("http://localhost:8080/test/include.html");
 		
 		// TODO: Assert innerHTML rather than text
 		expect(browser.driver.findElement(by.id("include")).getText()).toEqual("y");
