b1aadb79a4587fce75a6daec9a76145a76c763ff
Mark Hobson <mark.hobson@blackpepper.co.uk>
2015-12-23 16:57:16 +0000

    Issue #3: Switched tests to use Protractor

--- test/thymeleaf-fragment-test.js
+++ test/thymeleaf-fragment-test.js
@@ -1,4 +1,10 @@
-QUnit.test("th:include test", function(assert) {
-	var include = document.getElementById("include");
-	assert.equal(include.innerHTML, "<p th:fragment=\"x\">y</p>");
+describe("thymeleaf-fragment.js", function() {
+	
+	it("should include fragments", function() {
+		browser.driver.get("http://localhost:8080/test/thymeleaf-fragment-test.html");
+		
+		// TODO: Assert innerHTML rather than text
+		expect(browser.driver.findElement(by.id("include")).getText()).toEqual("y");
+	});
+	
 });
