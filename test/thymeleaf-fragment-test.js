1415e1f1867d2183d1f098007fe0be182b1ba276
lknop <lukasz.knop@eo.pl>
2016-10-31 14:04:01 +0100

    add tests for recursive fragment processing

--- test/thymeleaf-fragment-test.js
+++ test/thymeleaf-fragment-test.js
@@ -6,5 +6,13 @@ describe("thymeleaf-fragment.js", function() {
 		// TODO: Assert innerHTML rather than text
 		expect(browser.driver.findElement(by.id("include")).getText()).toEqual("y");
 	});
+
+	it("should include fragments recursively and stop at maxLevel depth", function() {
+		browser.driver.get("http://localhost:8080/test/recursion.html");
+		browser.wait(function() {return browser.driver.isElementPresent(by.id('f5'))}, 5000);
+		var text = browser.driver.findElement(by.id("include")).getText();
+		expect(text).toContain("fragment5");
+		expect(text).not.toContain("fragment6");
+	});
 	
 });
