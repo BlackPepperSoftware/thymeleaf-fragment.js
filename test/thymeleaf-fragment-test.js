bd0fa4cb909c32410c04323d30e827ed953e370b
lknop <lukasz.knop@eo.pl>
2016-11-01 17:34:38 +0100

    add support for DOM selectors

--- test/thymeleaf-fragment-test.js
+++ test/thymeleaf-fragment-test.js
@@ -1,5 +1,10 @@
 describe("thymeleaf-fragment.js", function() {
-	
+
+	it("should include fragments with DOM selectors", function() {
+		browser.driver.get("http://localhost:8080/test/domselectors.html");
+		expect(browser.driver.findElement(by.id("include")).getText()).toEqual("y");
+	});
+
 	it("should include fragments", function() {
 		browser.driver.get("http://localhost:8080/test/include.html");
 		
