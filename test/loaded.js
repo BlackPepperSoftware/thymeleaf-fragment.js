476dbf80cad755064263903e190786355f27f801
Mark Hobson <mark.hobson@blackpepper.co.uk>
2016-12-09 16:22:39 +0000

    Wait for fragments to be loaded before test assertions

---
+++ test/loaded.js
@@ -0,0 +1,4 @@
+$(function() {
+    // signal to the tests that the fragments are considered to be loaded
+    $('body').addClass('loaded');
+});
--- test/thymeleaf-fragment-test.js
+++ test/thymeleaf-fragment-test.js
@@ -1,15 +1,28 @@
 describe('thymeleaf-fragment.js', function() {
 	
 	it('should include fragments', function() {
-		browser.driver.get('http://localhost:8080/test/include.html');
+		load('include.html');
 		
 		expect(browser.driver.findElement(by.id('include')).getInnerHtml()).toEqual('<p th:fragment="x">y</p>');
 	});
 
 	it('should replace fragments', function() {
-		browser.driver.get('http://localhost:8080/test/replace.html');
+		load('replace.html');
 
 		expect(browser.driver.findElement(by.id('replace')).getInnerHtml()).toEqual('<p th:fragment="x">y</p>');
 	});
+
+	var load = function(file) {
+		browser.driver.get('http://localhost:8080/test/' + file);
+		browser.wait(untilElementPresent(by.css('body.loaded')), 1000);
+	};
+
+	var untilElementPresent = function(locator) {
+		return function() {
+			return browser.driver.findElements(locator).then(function(elements) {
+				return !!elements.length;
+			})
+		}
+	};
 
 });
