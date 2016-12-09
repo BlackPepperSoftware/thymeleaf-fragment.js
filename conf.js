649c9b867f8d83a62e570f27d64c50914e9f24f7
Mark Hobson <mark.hobson@blackpepper.co.uk>
2016-12-09 12:27:39 +0000

    Upgraded Selenium to 2.53.1

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 exports.config = {
 	framework: "jasmine",
-	seleniumServerJar: "./node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-2.48.2.jar",
+	seleniumServerJar: "./node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-2.53.1.jar",
 	chromeDriver: "./node_modules/chromedriver/lib/chromedriver/chromedriver",
 	specs: ["test/thymeleaf-fragment-test.js"]
 };
