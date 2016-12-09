c124d953f55faf3aa8a65217190f3c3bd406b7a2
Mark Hobson <mark.hobson@blackpepper.co.uk>
2016-12-09 15:07:03 +0000

    Added th:replace test

--- test/thymeleaf-fragment-test.js
+++ test/thymeleaf-fragment-test.js
@@ -5,5 +5,11 @@ describe('thymeleaf-fragment.js', function() {
 		
 		expect(browser.driver.findElement(by.id('include')).getInnerHtml()).toEqual('<p th:fragment="x">y</p>');
 	});
-	
+
+	it('should replace fragments', function() {
+		browser.driver.get('http://localhost:8080/test/replace.html');
+
+		expect(browser.driver.findElement(by.id('replace')).getInnerHtml()).toEqual('<p th:fragment="x">y</p>');
+	});
+
 });
