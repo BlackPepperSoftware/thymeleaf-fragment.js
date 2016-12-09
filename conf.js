a372d40082e3f16c1716b7c64050a56b035487b7
Mark Hobson <mark.hobson@blackpepper.co.uk>
2016-12-09 13:58:33 +0000

    Upgraded Selenium to 3.0.1

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 exports.config = {
 	framework: "jasmine",
-	seleniumServerJar: "./node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-2.53.1.jar",
+	seleniumServerJar: "./node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-3.0.1.jar",
 	chromeDriver: "./node_modules/chromedriver/lib/chromedriver/chromedriver",
 	specs: ["test/thymeleaf-fragment-test.js"]
 };
