06b5790003d61db9f48f91dd63a9c116a815abcf
Mark Hobson <mark.hobson@blackpepper.co.uk>
2016-12-09 14:54:50 +0000

    Assert on inner HTML rather than text

--- test/thymeleaf-fragment-test.js
+++ test/thymeleaf-fragment-test.js
@@ -3,8 +3,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should include fragments', function() {
 		browser.driver.get('http://localhost:8080/test/include.html');
 		
-		// TODO: Assert innerHTML rather than text
-		expect(browser.driver.findElement(by.id('include')).getText()).toEqual('y');
+		expect(browser.driver.findElement(by.id('include')).getInnerHtml()).toEqual('<p th:fragment="x">y</p>');
 	});
 	
 });
