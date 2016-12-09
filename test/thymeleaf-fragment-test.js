36f97d460009faa412d46829b746d4d634db71d1
Mark Hobson <mark.hobson@blackpepper.co.uk>
2016-12-09 14:53:34 +0000

    Switched quote style to single

--- test/thymeleaf-fragment-test.js
+++ test/thymeleaf-fragment-test.js
@@ -1,10 +1,10 @@
-describe("thymeleaf-fragment.js", function() {
+describe('thymeleaf-fragment.js', function() {
 	
-	it("should include fragments", function() {
-		browser.driver.get("http://localhost:8080/test/include.html");
+	it('should include fragments', function() {
+		browser.driver.get('http://localhost:8080/test/include.html');
 		
 		// TODO: Assert innerHTML rather than text
-		expect(browser.driver.findElement(by.id("include")).getText()).toEqual("y");
+		expect(browser.driver.findElement(by.id('include')).getText()).toEqual('y');
 	});
 	
 });
--- thymeleaf-fragment.js
+++ thymeleaf-fragment.js
@@ -6,10 +6,10 @@
 function ThymeleafFragment() {
 
 	// Prefix that gets prepended to view names when building a URL
-	var templatePrefix = "";
+	var templatePrefix = '';
 
 	// Suffix that gets appended to view names when building a URL
-	var templateSuffix = ".html";
+	var templateSuffix = '.html';
 
 	this.processAttributes = function() {
 		// Hold off scripts waiting for the document to be ready
@@ -17,14 +17,14 @@ function ThymeleafFragment() {
 		
 		var promises = [];
 		
-		$("[th\\:include]").each(function() {
-			var fragmentSpec = $(this).attr("th:include");
+		$('[th\\:include]').each(function() {
+			var fragmentSpec = $(this).attr('th:include');
 			var fragmentUri = resolveFragmentUri(fragmentSpec);
 			promises.push(createLoadPromise(this, fragmentUri, false));
 		});
 		
-		$("[th\\:replace]").each(function() {
-			var fragmentSpec = $(this).attr("th:replace");
+		$('[th\\:replace]').each(function() {
+			var fragmentSpec = $(this).attr('th:replace');
 			var fragmentUri = resolveFragmentUri(fragmentSpec);
 			promises.push(createLoadPromise(this, fragmentUri, true));
 		});
@@ -36,23 +36,23 @@ function ThymeleafFragment() {
 	};
 	
 	var resolveFragmentUri = function(fragmentSpec) {
-		var tokens = fragmentSpec.split("::");
+		var tokens = fragmentSpec.split('::');
 		var templateName = tokens[0];
 		var fragmentName = removeFragmentParameters(tokens[1]);
 		
 		var resourceName = resolveTemplate(templateName);
-		var fragmentSelector = "[th\\:fragment^='" + fragmentName + "']";
+		var fragmentSelector = '[th\\:fragment^="' + fragmentName + '"]';
 
-		return resourceName + " " + fragmentSelector;
+		return resourceName + ' ' + fragmentSelector;
 	};
 
 	var removeFragmentParameters = function(fragmentExpression) {
-		var index = fragmentExpression.indexOf("(");
+		var index = fragmentExpression.indexOf('(');
 		return (index == -1) ? fragmentExpression : fragmentExpression.substr(0, index);
 	};
 
 	var resolveTemplate = function(templateName) {
-		var link = document.createElement("a");
+		var link = document.createElement('a');
 		link.href = templatePrefix + templateName + templateSuffix;
 		return link.href;
 	};
