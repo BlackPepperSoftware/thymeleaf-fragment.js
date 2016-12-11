b4118ca1094efd650a81a1e037ca0530b5f07b67
Mark Hobson <markhobson@gmail.com>
2016-12-11 12:30:43 +0000

    Improved test fragment names

--- test/thymeleaf-fragment-test.js
+++ test/thymeleaf-fragment-test.js
@@ -3,16 +3,18 @@ describe('thymeleaf-fragment.js', function() {
 	it('should include fragments', function() {
 		load('include.html');
 		
-		expect(browser.driver.findElement(by.id('include')).getInnerHtml()).toEqual('<div th:fragment="x">y</div>');
+		expect(browser.driver.findElement(by.id('include')).getInnerHtml()).toEqual(
+			'<div th:fragment="simple">x</div>'
+		);
 	});
 
 	it('should include nested fragments', function() {
 		load('include-nested.html');
 
 		expect(browser.driver.findElement(by.id('include')).getInnerHtml()).toEqual(
-			'<div th:fragment="y">' +
-				'<div th:include="fragments::x">' +
-					'<div th:fragment="x">y</div>' +
+			'<div th:fragment="nestedInclude">' +
+				'<div th:include="fragments::simple">' +
+					'<div th:fragment="simple">x</div>' +
 				'</div>' +
 			'</div>'
 		);
@@ -21,21 +23,25 @@ describe('thymeleaf-fragment.js', function() {
 	it('should ignore whitespace in include fragment spec', function() {
 		load('include-whitespace.html');
 		
-		expect(browser.driver.findElement(by.id('include')).getInnerHtml()).toEqual('<div th:fragment="x">y</div>');
+		expect(browser.driver.findElement(by.id('include')).getInnerHtml()).toEqual(
+			'<div th:fragment="simple">x</div>'
+		);
 	});
 	
 	it('should replace fragments', function() {
 		load('replace.html');
 
-		expect(browser.driver.findElement(by.id('replace')).getInnerHtml()).toEqual('<div th:fragment="x">y</div>');
+		expect(browser.driver.findElement(by.id('replace')).getInnerHtml()).toEqual(
+			'<div th:fragment="simple">x</div>'
+		);
 	});
 
 	it('should replace nested fragments', function() {
 		load('replace-nested.html');
 
 		expect(browser.driver.findElement(by.id('replace')).getInnerHtml()).toEqual(
-			'<div th:fragment="z">' +
-				'<div th:fragment="x">y</div>' +
+			'<div th:fragment="nestedReplace">' +
+				'<div th:fragment="simple">x</div>' +
 			'</div>'
 		);
 	});
@@ -43,7 +49,9 @@ describe('thymeleaf-fragment.js', function() {
 	it('should ignore whitespace in replace fragment spec', function() {
 		load('replace-whitespace.html');
 		
-		expect(browser.driver.findElement(by.id('replace')).getInnerHtml()).toEqual('<div th:fragment="x">y</div>');
+		expect(browser.driver.findElement(by.id('replace')).getInnerHtml()).toEqual(
+			'<div th:fragment="simple">x</div>'
+		);
 	});
 
 	var load = function(file) {
