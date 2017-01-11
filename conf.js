3582a681ed0354b5664a8898c9bc5ae0e5377c0c
Mark Hobson <mark.hobson@blackpepper.co.uk>
2017-01-11 14:49:07 +0000

    Renamed test to *.spec.js

--- conf.js
+++ conf.js
@@ -2,5 +2,5 @@ exports.config = {
 	framework: 'jasmine',
 	seleniumServerJar: './node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-3.0.1.jar',
 	chromeDriver: './node_modules/chromedriver/lib/chromedriver/chromedriver',
-	specs: ['test/thymeleaf-fragment-test.js']
+	specs: ['test/thymeleaf-fragment.spec.js']
 };
