e6fd475dd39b77354bf466b913faf17cfe2254a2
Mark Hobson <mark.hobson@blackpepper.co.uk>
2017-01-11 15:56:08 +0000

    Introduced convenience method to get test result

--- test/thymeleaf-fragment.spec.js
+++ test/thymeleaf-fragment.spec.js
@@ -3,7 +3,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should include fragments by fragment name', function() {
 		load('include-fragmentname.html');
 		
-		expect(browser.driver.findElement(by.tagName('main')).getInnerHtml()).toEqual(
+		expect(getMainInnerHtml()).toEqual(
 			'<div th:include="fragments::simple">' +
 				'<div th:fragment="simple">x</div>' +
 			'</div>'
@@ -13,7 +13,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should include fragments by fragment name with parameters', function() {
 		load('include-fragmentparams.html');
 		
-		expect(browser.driver.findElement(by.tagName('main')).getInnerHtml()).toEqual(
+		expect(getMainInnerHtml()).toEqual(
 			'<div th:include="fragments::simple(a=b,c=d)">' +
 				'<div th:fragment="simple">x</div>' +
 			'</div>'
@@ -23,7 +23,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should include fragments by explicit DOM selector', function() {
 		load('include-domselector-explicit.html');
 		
-		expect(browser.driver.findElement(by.tagName('main')).getInnerHtml()).toEqual(
+		expect(getMainInnerHtml()).toEqual(
 			'<div th:include="fragments::[#simple]">' +
 				'<div id="simple">y</div>' +
 			'</div>'
@@ -33,7 +33,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should include fragments by implicit DOM selector', function() {
 		load('include-domselector-implicit.html');
 		
-		expect(browser.driver.findElement(by.tagName('main')).getInnerHtml()).toEqual(
+		expect(getMainInnerHtml()).toEqual(
 			'<div th:include="fragments::#simple">' +
 				'<div id="simple">y</div>' +
 			'</div>'
@@ -43,7 +43,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should include templates', function() {
 		load('include-template.html');
 		
-		expect(browser.driver.findElement(by.tagName('main')).getInnerHtml()).toEqual(
+		expect(getMainInnerHtml()).toEqual(
 			'<div th:include="template">' +
 				'<div>z</div>' +
 			'</div>'
@@ -53,7 +53,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should include nested fragments', function() {
 		load('include-nested.html');
 
-		expect(browser.driver.findElement(by.tagName('main')).getInnerHtml()).toEqual(
+		expect(getMainInnerHtml()).toEqual(
 			'<div th:include="fragments::nestedInclude">' +
 				'<div th:fragment="nestedInclude">' +
 					'<div th:include="fragments::simple">' +
@@ -67,7 +67,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should ignore whitespace in include fragment spec', function() {
 		load('include-whitespace.html');
 		
-		expect(browser.driver.findElement(by.tagName('main')).getInnerHtml()).toEqual(
+		expect(getMainInnerHtml()).toEqual(
 			'<div th:include=" fragments :: simple ">' +
 				'<div th:fragment="simple">x</div>' +
 			'</div>'
@@ -77,7 +77,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should replace fragments by fragment name', function() {
 		load('replace-fragmentname.html');
 
-		expect(browser.driver.findElement(by.tagName('main')).getInnerHtml()).toEqual(
+		expect(getMainInnerHtml()).toEqual(
 			'<div th:fragment="simple">x</div>'
 		);
 	});
@@ -85,7 +85,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should replace fragments by fragment name with parameters', function() {
 		load('replace-fragmentparams.html');
 
-		expect(browser.driver.findElement(by.tagName('main')).getInnerHtml()).toEqual(
+		expect(getMainInnerHtml()).toEqual(
 			'<div th:fragment="simple">x</div>'
 		);
 	});
@@ -93,7 +93,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should replace fragments by explicit DOM selector', function() {
 		load('replace-domselector-explicit.html');
 		
-		expect(browser.driver.findElement(by.tagName('main')).getInnerHtml()).toEqual(
+		expect(getMainInnerHtml()).toEqual(
 			'<div id="simple">y</div>'
 		);
 	});
@@ -101,7 +101,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should replace fragments by implicit DOM selector', function() {
 		load('replace-domselector-implicit.html');
 		
-		expect(browser.driver.findElement(by.tagName('main')).getInnerHtml()).toEqual(
+		expect(getMainInnerHtml()).toEqual(
 			'<div id="simple">y</div>'
 		);
 	});
@@ -109,7 +109,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should replace templates', function() {
 		load('replace-template.html');
 		
-		expect(browser.driver.findElement(by.tagName('main')).getInnerHtml()).toEqual(
+		expect(getMainInnerHtml()).toEqual(
 			'<div>z</div>'
 		);
 	});
@@ -117,7 +117,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should replace nested fragments', function() {
 		load('replace-nested.html');
 
-		expect(browser.driver.findElement(by.tagName('main')).getInnerHtml()).toEqual(
+		expect(getMainInnerHtml()).toEqual(
 			'<div th:fragment="nestedReplace">' +
 				'<div th:fragment="simple">x</div>' +
 			'</div>'
@@ -127,7 +127,7 @@ describe('thymeleaf-fragment.js', function() {
 	it('should ignore whitespace in replace fragment spec', function() {
 		load('replace-whitespace.html');
 		
-		expect(browser.driver.findElement(by.tagName('main')).getInnerHtml()).toEqual(
+		expect(getMainInnerHtml()).toEqual(
 			'<div th:fragment="simple">x</div>'
 		);
 	});
@@ -143,6 +143,10 @@ describe('thymeleaf-fragment.js', function() {
 				return !!elements.length;
 			})
 		}
+	};
+	
+	var getMainInnerHtml = function() {
+		return browser.driver.findElement(by.tagName('main')).getInnerHtml();	
 	};
 
 });
