3655a9f7ba8e45759e4195ad160bd530460891f4
Mark Hobson <mark.hobson@blackpepper.co.uk>
2017-01-11 15:57:41 +0000

    Grouped tests by tag

--- test/thymeleaf-fragment.spec.js
+++ test/thymeleaf-fragment.spec.js
@@ -1,125 +1,133 @@
 describe('thymeleaf-fragment.js', function() {
 	
-	it('should include fragments by fragment name', function() {
-		load('include-fragmentname.html');
-		
-		expect(getMainInnerHtml()).toEqual(
-			'<div th:include="fragments::simple">' +
-				'<div th:fragment="simple">x</div>' +
-			'</div>'
-		);
-	});
+	describe('th:include', function() {
 
-	it('should include fragments by fragment name with parameters', function() {
-		load('include-fragmentparams.html');
-		
-		expect(getMainInnerHtml()).toEqual(
-			'<div th:include="fragments::simple(a=b,c=d)">' +
-				'<div th:fragment="simple">x</div>' +
-			'</div>'
-		);
-	});
-	
-	it('should include fragments by explicit DOM selector', function() {
-		load('include-domselector-explicit.html');
-		
-		expect(getMainInnerHtml()).toEqual(
-			'<div th:include="fragments::[#simple]">' +
-				'<div id="simple">y</div>' +
-			'</div>'
-		);
-	});
-	
-	it('should include fragments by implicit DOM selector', function() {
-		load('include-domselector-implicit.html');
-		
-		expect(getMainInnerHtml()).toEqual(
-			'<div th:include="fragments::#simple">' +
-				'<div id="simple">y</div>' +
-			'</div>'
-		);
-	});
-	
-	it('should include templates', function() {
-		load('include-template.html');
-		
-		expect(getMainInnerHtml()).toEqual(
-			'<div th:include="template">' +
-				'<div>z</div>' +
-			'</div>'
-		);
-	});
-	
-	it('should include nested fragments', function() {
-		load('include-nested.html');
+		it('should include fragments by fragment name', function() {
+			load('include-fragmentname.html');
 
-		expect(getMainInnerHtml()).toEqual(
-			'<div th:include="fragments::nestedInclude">' +
-				'<div th:fragment="nestedInclude">' +
-					'<div th:include="fragments::simple">' +
-						'<div th:fragment="simple">x</div>' +
+			expect(getMainInnerHtml()).toEqual(
+				'<div th:include="fragments::simple">' +
+					'<div th:fragment="simple">x</div>' +
+				'</div>'
+			);
+		});
+
+		it('should include fragments by fragment name with parameters', function() {
+			load('include-fragmentparams.html');
+
+			expect(getMainInnerHtml()).toEqual(
+				'<div th:include="fragments::simple(a=b,c=d)">' +
+					'<div th:fragment="simple">x</div>' +
+				'</div>'
+			);
+		});
+
+		it('should include fragments by explicit DOM selector', function() {
+			load('include-domselector-explicit.html');
+
+			expect(getMainInnerHtml()).toEqual(
+				'<div th:include="fragments::[#simple]">' +
+					'<div id="simple">y</div>' +
+				'</div>'
+			);
+		});
+
+		it('should include fragments by implicit DOM selector', function() {
+			load('include-domselector-implicit.html');
+
+			expect(getMainInnerHtml()).toEqual(
+				'<div th:include="fragments::#simple">' +
+					'<div id="simple">y</div>' +
+				'</div>'
+			);
+		});
+
+		it('should include templates', function() {
+			load('include-template.html');
+
+			expect(getMainInnerHtml()).toEqual(
+				'<div th:include="template">' +
+					'<div>z</div>' +
+				'</div>'
+			);
+		});
+
+		it('should include nested fragments', function() {
+			load('include-nested.html');
+
+			expect(getMainInnerHtml()).toEqual(
+				'<div th:include="fragments::nestedInclude">' +
+					'<div th:fragment="nestedInclude">' +
+						'<div th:include="fragments::simple">' +
+							'<div th:fragment="simple">x</div>' +
+						'</div>' +
 					'</div>' +
-				'</div>' +
-			'</div>'
-		);
-	});
-	
-	it('should ignore whitespace in include fragment spec', function() {
-		load('include-whitespace.html');
-		
-		expect(getMainInnerHtml()).toEqual(
-			'<div th:include=" fragments :: simple ">' +
-				'<div th:fragment="simple">x</div>' +
-			'</div>'
-		);
-	});
-	
-	it('should replace fragments by fragment name', function() {
-		load('replace-fragmentname.html');
+				'</div>'
+			);
+		});
 
-		expect(getMainInnerHtml()).toEqual('<div th:fragment="simple">x</div>');
-	});
+		it('should ignore whitespace in include fragment spec', function() {
+			load('include-whitespace.html');
 
-	it('should replace fragments by fragment name with parameters', function() {
-		load('replace-fragmentparams.html');
+			expect(getMainInnerHtml()).toEqual(
+				'<div th:include=" fragments :: simple ">' +
+					'<div th:fragment="simple">x</div>' +
+				'</div>'
+			);
+		});
 
-		expect(getMainInnerHtml()).toEqual('<div th:fragment="simple">x</div>');
 	});
 	
-	it('should replace fragments by explicit DOM selector', function() {
-		load('replace-domselector-explicit.html');
-		
-		expect(getMainInnerHtml()).toEqual('<div id="simple">y</div>');
-	});
-	
-	it('should replace fragments by implicit DOM selector', function() {
-		load('replace-domselector-implicit.html');
-		
-		expect(getMainInnerHtml()).toEqual('<div id="simple">y</div>');
-	});
-	
-	it('should replace templates', function() {
-		load('replace-template.html');
-		
-		expect(getMainInnerHtml()).toEqual('<div>z</div>');
-	});
-	
-	it('should replace nested fragments', function() {
-		load('replace-nested.html');
+	describe('th:replace', function() {
 
-		expect(getMainInnerHtml()).toEqual(
-			'<div th:fragment="nestedReplace">' +
-				'<div th:fragment="simple">x</div>' +
-			'</div>'
-		);
+		it('should replace fragments by fragment name', function() {
+			load('replace-fragmentname.html');
+
+			expect(getMainInnerHtml()).toEqual('<div th:fragment="simple">x</div>');
+		});
+
+		it('should replace fragments by fragment name with parameters', function() {
+			load('replace-fragmentparams.html');
+
+			expect(getMainInnerHtml()).toEqual('<div th:fragment="simple">x</div>');
+		});
+
+		it('should replace fragments by explicit DOM selector', function() {
+			load('replace-domselector-explicit.html');
+
+			expect(getMainInnerHtml()).toEqual('<div id="simple">y</div>');
+		});
+
+		it('should replace fragments by implicit DOM selector', function() {
+			load('replace-domselector-implicit.html');
+
+			expect(getMainInnerHtml()).toEqual('<div id="simple">y</div>');
+		});
+
+		it('should replace templates', function() {
+			load('replace-template.html');
+
+			expect(getMainInnerHtml()).toEqual('<div>z</div>');
+		});
+
+		it('should replace nested fragments', function() {
+			load('replace-nested.html');
+
+			expect(getMainInnerHtml()).toEqual(
+				'<div th:fragment="nestedReplace">' +
+					'<div th:fragment="simple">x</div>' +
+				'</div>'
+			);
+		});
+
+		it('should ignore whitespace in replace fragment spec', function() {
+			load('replace-whitespace.html');
+
+			expect(getMainInnerHtml()).toEqual('<div th:fragment="simple">x</div>');
+		});
+
 	});
 	
-	it('should ignore whitespace in replace fragment spec', function() {
-		load('replace-whitespace.html');
-		
-		expect(getMainInnerHtml()).toEqual('<div th:fragment="simple">x</div>');
-	});
-
 	var load = function(file) {
 		browser.driver.get('http://localhost:8080/test/' + file);
 		browser.wait(untilElementPresent(by.css('body.loaded')), 1000);
