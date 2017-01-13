b34e160dd0d240e1c427b420b506416398499770
Mark Hobson <mark.hobson@blackpepper.co.uk>
2017-01-13 11:41:05 +0000

    Use Protractor to install Selenium and ChromeDriver

--- conf.js
+++ conf.js
@@ -1,6 +1,4 @@
 exports.config = {
 	framework: 'jasmine',
-	seleniumServerJar: './node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-3.0.1.jar',
-	chromeDriver: './node_modules/chromedriver/lib/chromedriver/chromedriver',
 	specs: ['test/thymeleaf-fragment.spec.js']
 };
